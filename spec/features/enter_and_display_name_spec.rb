f61a9187e0052edd4a735a9f6f669b672ffca3bf
Rob Oman <rob.oman@icloud.com>
2022-03-08 17:09:36 +0000

    refactor
    Co-authored-by: Jasmine Cheung <jazzc001@users.noreply.github.com>

--- spec/features/enter_and_display_name_spec.rb
+++ spec/features/enter_and_display_name_spec.rb
@@ -1,16 +1,4 @@
 feature 'Player entry form' do
-  scenario 'Player 1 name field is visible' do
-    visit('/')
-    expect(page).to have_field(name: 'player_1')
-  end
-  scenario 'Player 2 name field is visible' do
-    visit('/')
-    expect(page).to have_field(name: 'player_2')
-  end
-  scenario 'Submit button visible' do
-    visit('/')
-    expect(page).to have_button('Submit')
-  end
   scenario 'Form is submitted and names appear on /play' do
     visit('/')
     fill_in(name: 'player_1', with: 'Rob')
